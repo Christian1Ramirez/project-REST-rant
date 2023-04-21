2f9a15b740b978c90bdfb3b1b50d5be2ce08d153
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-04-20 19:29:15 -0700

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,7 +6,13 @@ function error404 () {
    <Def>
        <main>
            <h1>404: PAGE NOT FOUND</h1>
-           <p>Oops, sorry, we can't find this page!</p>
+           <p>This page has ghosted you!</p>
+           <div>
+                 <img src="images/ghosted.jpg" alt="Multiple bean tacos" width="330" height="500"/>
+                <div>
+                   Photo by <a href="https://unsplash.com/photos/8Ef813JYl3U">Bahnijit Barman</a> on <a href="https://unsplash.com/s/photos/free">Unsplash</a>
+                </div>
+                </div>
        </main>
    </Def>
  )
--- views/home.jsx
+++ views/home.jsx
@@ -2,16 +2,22 @@ const React = require('react')
 const Def = require('./default')
 
 function home () {
- return (
-   <Def>
-       <main>
-           <h1>HOME</h1>
-       </main>
-    <a href="/places">
-      <button className="btn-primary">Places Page</button>
-    </a>
-   </Def>
- )
+    return (
+        <Def>
+            <main>
+                <h1>HOME</h1>
+                <div>
+                 <img src="images/bean_tacos.jpg" alt="Multiple bean tacos" width="300"/>
+                <div>
+                   Photo by <a href="https://unsplash.com/photos/J04BD4ysoh8">Edgar Castrejon</a> on <a href="https://unsplash.com/s/photos/free">Unsplash</a>
+                </div>
+                </div>
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
+            </main>
+        </Def>
+    )
 }
 
 module.exports = home
