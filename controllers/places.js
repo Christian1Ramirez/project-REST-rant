5ff2d039d485de34db782aca9736c3b69bc6fa7d
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-04-22 12:44:22 -0700

    Navbar with style

--- controllers/places.js
+++ controllers/places.js
@@ -25,17 +25,4 @@ router.post('/', (req, res) => {
   res.render('places/new')
 })
 
-// // GET /places/:id
-// router.get('/:id', (req, res) => {
-//   let place = {
-//     id: req.params.id,
-//     name: 'H-Thai-ML',
-//     city: 'Seattle',
-//     state: 'WA',
-//     cuisines: 'Thai, Pan-Asian',
-//     pic: '../images/thai_rest.jpg'
-//   }
-//   res.render('places/show', { place })
-// })
-
 module.exports = router
