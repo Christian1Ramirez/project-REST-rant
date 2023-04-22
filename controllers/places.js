72556c756b25106f76b5889d0eb79f45ccfb2d3b
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-04-21 17:37:46 -0700

    Jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -23,17 +23,17 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
-// // GET /places/:id
-// router.get('/:id', (req, res) => {
-//   let place = {
-//     id: req.params.id,
-//     name: 'H-Thai-ML',
-//     city: 'Seattle',
-//     state: 'WA',
-//     cuisines: 'Thai, Pan-Asian',
-//     pic: '../images/thai_rest.jpg'
-//   }
-//   res.render('places/show', { place })
-// })
+// GET /places/:id
+router.get('/:id', (req, res) => {
+  let place = {
+    id: req.params.id,
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '../images/thai_rest.jpg'
+  }
+  res.render('places/show', { place })
+})
 
 module.exports = router
