854d16ae0dd68dd35f1d425fc8c1253e4cebe799
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-04-21 22:02:39 -0700

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,44 +1,41 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
-// GET /places
 router.get('/', (req, res) => {
-  let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '../images/thai_rest.jpg'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '../images/cafe.jpg'
-  }]
-  res.render('places/index', { places })
+    res.render('places/index', { places })
 })
 
 router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
   })
-
+  
 // GET /places/new
-router.get('/new', (req, res) => {
+  router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
-// GET /places/:id
-router.get('/:id', (req, res) => {
-  let place = {
-    id: req.params.id,
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '../images/thai_rest.jpg'
-  }
-  res.render('places/show', { place })
-})
+// // GET /places/:id
+// router.get('/:id', (req, res) => {
+//   let place = {
+//     id: req.params.id,
+//     name: 'H-Thai-ML',
+//     city: 'Seattle',
+//     state: 'WA',
+//     cuisines: 'Thai, Pan-Asian',
+//     pic: '../images/thai_rest.jpg'
+//   }
+//   res.render('places/show', { place })
+// })
 
 module.exports = router
